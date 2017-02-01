521fa765f49f70d5cd2e5cb3cf2ac3ccdfa40978
fetoso <victorjvillamil@gmail.com>
2017-02-01 17:08:55 -0400

    Cleaning adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -6,28 +6,6 @@ export default DS.RESTAdapter.extend(DataAdapterMixin, {
   namespace: 'api',
   authorizer: 'authorizer:oauth2',
   host: config.APP.apiURL,
-  // serializer: DS.RESTSerializer.extend({
-  //   extract: function(loader, json, type, record) {
-  //     var newJSON,
-  //         root;
-  //     root = this.rootForType(type);
-  //     newJSON = {};
-  //     newJSON[root] = json;
-  //     json = newJSON;
-  //
-  //     this.sideload(loader, type, json, root);
-  //     this.extractMeta(loader, type, json);
-  //
-  //     if (json[root]) {
-  //       if (record) {
-  //         loader.updateId(record, json[root]);
-  //       }
-  //       return this.extractRecordRepresentation(loader, type, json[root]);
-  //     } else {
-  //       return Ember.Logger.warn("Extract requested, but no data given for " + type + ". This may cause weird problems.");
-  //     }
-  //   },
-  // })
 
 });
 
