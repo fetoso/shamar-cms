a6dd9135e19b8ef17ba1267227f0001c5115392e
fetoso <victorjvillamil@gmail.com>
2017-02-01 18:48:49 -0400

    Testing reverted

--- app/routes/categories.js
+++ app/routes/categories.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
-// import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
 
   model: function () {
     return Ember.RSVP.hash({
@@ -12,7 +12,7 @@ export default Ember.Route.extend({
   setupController: function(controller, models) {
     return controller.setProperties(models);
   },
-  // session: Ember.inject.service('session'),
+  session: Ember.inject.service('session'),
   actions: {
     deleteCategory: function(category) {
       if (confirm("Are you sure you want to Delete this item?")) {
