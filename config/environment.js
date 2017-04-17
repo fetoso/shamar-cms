1b28a705fd65a23875e04b62272eab410cfbe4ef
fetoso <victorjvillamil@gmail.com>
2017-04-17 17:46:51 -0400

    Removing locationType hash

--- config/environment.js
+++ config/environment.js
@@ -85,7 +85,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.locationType = 'hash';
+    // ENV.locationType = 'hash';
     ENV.APP.usingCors = true;
 		ENV.APP.corsWithCreds = true;
 		ENV.APP.apiURL = 'http://shamar-dev.us-east-1.elasticbeanstalk.com';
