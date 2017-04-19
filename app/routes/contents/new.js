3eaad642a50529b6ee1de2ee4967e24b0306c2d1
fetoso <victorjvillamil@gmail.com>
2017-04-19 16:36:02 -0400

    Implementing brown strategy

--- app/routes/contents/new.js
+++ app/routes/contents/new.js
@@ -18,13 +18,30 @@ export default Ember.Route.extend({
   actions: {
     createContent: function(content) {
       var _this = this;
-      this.store.createRecord('content', content).save().then( function() {
-        console.log('pretty please!')
+      this.store.createRecord('content', content).save().then( function( response) {
+
+        console.log('pretty please!', response.id);
         // var t = _this.get('router.url');
         // var destination = t.substr(0, t.lastIndexOf("/"));
-        window.location.replace('/#/contents');
+        $('tbody').prepend(' \
+          <tr> \
+            <!-- <td class="hidden-xs">' + response.id + '</td> --> \
+            <td><a href="#" data-toggle="modal" data-target="#modal' + content.id + '">' + content.title +'</a></td> \
+            <td class="hidden-xs">' + content.notify + '</td> \
+            <td class="hidden-xs">' + content.is_active + '</td> \
+            <td> \
+              <div class="bs-component table-commands"> \
+                <div class="btn-group btn-group-justified"> \
+                  <a href="/#/contents/' + response.id + '" class="btn btn-default send ember-view">EDIT</a> \
+                  <!-- <a href="#" class="btn btn-default cancel" {{action "deleteContent" content}}>DELETE</a> --> \
+                </div> \
+              </div> \
+            </td> \
+          </tr>\
+        ');
+        // window.location.replace('/#/contents');
 
-        // _this.transitionTo('contents');
+        _this.transitionTo('contents');
       });
     }
   }
